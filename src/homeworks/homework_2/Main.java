7a080171256ba7dfce14ab19b222b209e6789103
Serhii Nikolaiev <snikolaiev@readdle.com>
2023-05-18 17:39:44 +0200

    fix

--- src/homeworks/homework_2/Main.java
+++ src/homeworks/homework_2/Main.java
@@ -1,7 +1,10 @@
 package homeworks.homework_2;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 
         homeworks.homework_2.class_1.SameName sameName1 = new homeworks.homework_2.class_1.SameName();
         System.out.println(sameName1.getClass());
@@ -22,5 +25,21 @@ public class Main {
 
         Car hyundai = new Car();
         hyundai.start();
+
+        //reflection
+        //private method startElectricity
+        Method startElectricityMethod = Car.class.getDeclaredMethod("startElectricity");
+        startElectricityMethod.setAccessible(true);
+        startElectricityMethod.invoke(hyundai);
+
+        //private method startCommand
+        Method startCommand = Car.class.getDeclaredMethod("startCommand");
+        startCommand.setAccessible(true);
+        startCommand.invoke(hyundai);
+
+        //private method startFuelSystem
+        Method startFuelSystem = Car.class.getDeclaredMethod("startFuelSystem");
+        startFuelSystem.setAccessible(true);
+        startFuelSystem.invoke(hyundai);
     }
 }
