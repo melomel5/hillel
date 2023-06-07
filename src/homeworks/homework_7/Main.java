6ae5da2340d5337e57fae723e7b1521fb739ce65
Serhii Nikolaiev <snikolaiev@readdle.com>
2023-06-08 00:56:11 +0200

    fix array values

--- src/homeworks/homework_7/Main.java
+++ src/homeworks/homework_7/Main.java
@@ -6,27 +6,12 @@ import homeworks.homework_7.exeptions.ArraySizeException;
 public class Main {
     public static void main(String[] args) {
 
-        String[][] array = new String[5][5];
-
-        array[0][0] = "0";
-        array[0][1] = "0";
-        array[0][2] = "0";
-        array[0][3] = "0";
-
-        array[1][0] = "1";
-        array[1][1] = "1";
-        array[1][2] = "1";
-        array[1][3] = "1";
-
-        array[2][0] = "2";
-        array[2][1] = "2";
-        array[2][2] = "2";
-        array[2][3] = "2";
-
-        array[3][0] = "3";
-        array[3][1] = "3";
-        array[3][2] = "3";
-        array[3][3] = "3";
+        String[][] array = {
+                { "1", "2", "3", "4" },
+                { "5", "6", "7", "8" },
+                { "9", "10", "11", "12" },
+                { "13", "14", "15", "16" }
+        };
 
         ArrayValueCalculator calculator = new ArrayValueCalculator();
 
